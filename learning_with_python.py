c80736ef6a14987fc482704da57cf8732e8bbdbc
hemanthreddy739 <programmercoursestodo@gmail.com>
2022-09-21 17:43:02 +0530

    some changes and additional stuff is added :)

--- learning_with_python.py
+++ learning_with_python.py
@@ -1,2 +1,9 @@
+# some changes and additional stuff is added :)
+print("it's time to learn git and github using python")
 
-print(" it's time to learn python")
+
+def tell_something_about_git():
+    print('git is a version control system')
+
+
+tell_something_about_git()
